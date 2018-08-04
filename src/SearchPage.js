d1052b9a977773675466d0834048f6496a8b1392
Dovydas Janulaitis <dovydas.janulaitis@gmail.com>
2018-08-04 15:37:26 +0300

    Create SearchPage functionality

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BookComponent from './BookComponent'
 
 class SearchPage extends Component {
   state = {
-    entry: ''
+    entry: '',
+    identifiedBooks: []
   }
 
   // method to update search entry in the search field
   updateEntry = (entry) => {
     this.setState({ entry: entry})
+
+    this.updateIdentifiedBooks(entry)
   }
+
+  updateIdentifiedBooks = (entry) => {
+    if(entry) {
+      BooksAPI.search(entry).then((identifiedBooks) => {
+        if(identifiedBooks.error) {
+          this.setState({ identifiedBooks: [] })
+        } else {
+          this.setState({ identifiedBooks: identifiedBooks})
+        }
+      })
+    } else {
+      this.setState({ identifiedBooks: [] })
+    }
+  }
+
 
   render() {
     return (
@@ -21,18 +41,24 @@ class SearchPage extends Component {
             >
           </Link>
           <div className="search-books-input-wrapper">
-
             <input
               type="text"
               placeholder="Search by title or author"
-              onChange={(event) => this.updateEntry(event.target.value)}
               value={this.state.entry}
-              />
-
+              onChange={(event) => this.updateEntry(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {this.state.identifiedBooks.map(identifiedBook => (
+              <li key={identifiedBook.id}>
+                <BookComponent
+                  book={identifiedBook}
+                />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
